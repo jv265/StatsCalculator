799942ecb903ded400f04fb6180faa6c9de7f2d7
Jordan Pisaniello <jp244@njit.edu>
2020-10-31 14:27:46 -0400

    feature: implemented 'random' list value with seed set

--- Generator/SetSeedSelectListValue.py
+++ Generator/SetSeedSelectListValue.py
@@ -1,2 +1,7 @@
 # Random Generator function #5
 # Set a seed and randomly select the same value from a list
+import random
+
+def set_seed_select_list_value(data):
+    random.seed(8)
+    return random.choice(data)
