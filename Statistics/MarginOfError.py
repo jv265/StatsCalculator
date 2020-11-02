906a3d05446428103d769187a1d5c68dae922659
joshvilson <jv265@njit.edu>
2020-11-01 23:42:30 -0500

    Feature: Added margin of error and correct issue with random sampling

--- Statistics/MarginOfError.py
+++ Statistics/MarginOfError.py
@@ -1,2 +1,12 @@
 from Statistics.StandardDeviation import standard_deviation
 from Statistics.ZScore import zscore
+from Calculator.Multiplication import multiplication
+from Calculator.Division import division
+from Calculator.SquareRoot import square_root
+
+
+def margin_of_error(sample, x):
+    zscore_result = zscore(sample, x)
+    sample_size = len(sample)
+    standard_deviation_result = standard_deviation(sample)
+    return multiplication(zscore_result, division(square_root(sample_size), standard_deviation_result))
--- Statistics/SimpleRandomSampling.py
+++ Statistics/SimpleRandomSampling.py
@@ -1,4 +1,5 @@
-from random import random
+import random
+
 
 def get_sample(data, sample_size):
-    return random.sample(data, k=sample_size)
+    return random.sample(data, sample_size)
