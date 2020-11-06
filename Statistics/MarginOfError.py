94de828e28e35a1bfe54c4750005d05475416f8c
joshvilson <jv265@njit.edu>
2020-11-06 18:06:01 -0500

    Fix: Margin of Error calculation fixed

--- Statistics/MarginOfError.py
+++ Statistics/MarginOfError.py
@@ -1,12 +1,27 @@
 from Statistics.StandardDeviation import standard_deviation
-from Statistics.ZScore import zscore
 from Calculator.Multiplication import multiplication
 from Calculator.Division import division
 from Calculator.SquareRoot import square_root
 
 
-def margin_of_error(sample, x):
-    zscore_result = zscore(sample, x)
+def margin_of_error(sample, confidence_level):
+    # Source: https://www.surveymonkey.com/mp/margin-of-error-calculator/
+    # First find our Z value from a confidence level
+    if confidence_level == 80:
+        z = 1.645
+    elif confidence_level == 85:
+        z = 1.645
+    elif confidence_level == 90:
+        z = 1.645
+    elif confidence_level == 95:
+        z = 1.96
+    elif confidence_level == 98:
+        z = 2.33
+    elif confidence_level == 99:
+        z = 2.58
+    else:
+        return 0
+
     sample_size = len(sample)
     standard_deviation_result = standard_deviation(sample)
-    return multiplication(zscore_result, division(square_root(sample_size), standard_deviation_result))
+    return multiplication(z, division(square_root(sample_size), standard_deviation_result))
