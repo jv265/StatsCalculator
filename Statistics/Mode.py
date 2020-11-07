0f7cf4720181a8c1166625790bd775314d28344d
joshvilson <jv265@njit.edu>
2020-11-06 21:58:53 -0500

    Task: Add comments for mode for clarity

--- Statistics/Mode.py
+++ Statistics/Mode.py
@@ -4,12 +4,15 @@ def mode(data):
     result_value = 1
     mode_found = False
 
+    # Goes through sample data and creates a dictionary of numbers with associated number of occurrences
     for num in data:
         if num in list_number_of_occurrences:
             list_number_of_occurrences[num] = list_number_of_occurrences[num] + 1
         else:
             list_number_of_occurrences[num] = 1
 
+    # For each number in the dictionary, checks if it's at least greater and adds.
+    # Otherwise we skip. We periodically check if we have a mode found during this process.
     for key in list_number_of_occurrences:
         if result_value < list_number_of_occurrences[key]:
             result_value = list_number_of_occurrences[key]
