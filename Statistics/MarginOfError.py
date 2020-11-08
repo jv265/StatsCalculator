f3f6320ae52eac6f780f9e06e4ebc3290f823f65
joshvilson <jv265@njit.edu>
2020-11-08 11:33:54 -0500

    Fix: corrections toward z value table for cochran, margin of error

--- Statistics/MarginOfError.py
+++ Statistics/MarginOfError.py
@@ -4,6 +4,7 @@ from Calculator.Division import division
 from Calculator.SquareRoot import square_root
 from Validation.Validations import empty_list_check
 from Validation.Validations import check_for_valid_numbers
+from Statistics import CalculateZValue
 
 
 def margin_of_error(sample, confidence_level):
@@ -11,23 +12,7 @@ def margin_of_error(sample, confidence_level):
     empty_list_check(sample)
     check_for_valid_numbers(sample)
 
-    # Source: https://www.surveymonkey.com/mp/margin-of-error-calculator/
-    # First find our Z value from a confidence level
-    if confidence_level == 80:
-        z = 1.645
-    elif confidence_level == 85:
-        z = 1.645
-    elif confidence_level == 90:
-        z = 1.645
-    elif confidence_level == 95:
-        z = 1.96
-    elif confidence_level == 98:
-        z = 2.33
-    elif confidence_level == 99:
-        z = 2.58
-    else:
-        return 0
-
+    z = CalculateZValue.calculate_zvalue(confidence_level)
     sample_size = len(sample)
     standard_deviation_result = standard_deviation(sample)
     return multiplication(z, division(square_root(sample_size), standard_deviation_result))
